189bd3cad11e6754c35ab32d1feaa0452b878240
David Bouhaben <davidbouhaben@yahoo.co.id>
2021-05-28 11:45:42 +0200

    connexion to MySQL database

---
+++ Backend/controller/user.js
@@ -0,0 +1,9 @@
+const models = require('../models');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+function signUp(req, res, next) {
+    const user = {
+        
+    }
+}
