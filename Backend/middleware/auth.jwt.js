4e60d31abe53d08b90020f491dd3a0376650f813
David Bouhaben <davidbouhaben@yahoo.co.id>
2021-05-29 13:21:23 +0200

    middleware authentification et authorisation

--- Backend/middleware/auth.jwt.js
+++ Backend/middleware/auth.jwt.js
@@ -4,3 +4,25 @@
 
 const jwt = require('jsonwebtoken')
 const config = require('../config/auth.config')
+const db = require('../models')
+const User = db.user
+
+verifyToken = (req, res, next) =>{
+    //On prends le tokken depuis l'entête de HTTP/ x-access-token 
+    let token = req.headers["x-access-token"];
+
+    if(!token){
+        return res.status(403).json({ message:'Désolé, il n\'y a aucun token trouvé'}
+        )
+    }
+}
+
+jwt.verify(token, config.secret, (err, decoded) => {
+    if(error) {
+        return res.status(401).json({
+            message: 'Vous n\'avez pas l\'authorisation'
+        })
+    }
+    req.userId = decoded.id
+    next()
+})
