348712db401a6d278ccaea8a86a69b17fcd68018
David Bouhaben <davidbouhaben@yahoo.co.id>
2021-06-18 14:49:13 +0200

    userId to idUser

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -55,11 +55,11 @@ exports.updateUser = (req, res) => {
 };
 
 exports.deleteMyAccount = (req, res) => {
-  userId = req.params.id
-  const idUser = req.params.userId; //l'id de user
-  console.log("userId :", userId, "idUser :", req.params.userId)
+  const deletedUser = req.params.id
+  const loggedUser = req.params.idUser; //l'id de user
+  console.log("deletedUser :", deletedUser, "loggedUser :", loggedUser)
 
-  if (idUser != null) {
+  if (loggedUser != null) {
     User.findOne({
       //On cherche une id d'utilisateur
       attributes: [
@@ -73,13 +73,13 @@ exports.deleteMyAccount = (req, res) => {
         "createdAt",
         "updatedAt",
       ],
-      where: { id: idUser }, //l'id de user est trouvé et compare avec l'id dans la base de données
+      where: { id: loggedUser }, //l'id de user est trouvé et compare avec l'id dans la base de données
     })
-      .then((user) => {
+      .then((userLogged) => {
         //après avoir trouvé l'id de user on cherche tous les id associé a l'id trouvé plus haut
         Post.findAll({
           where: {
-            idUser: postId,
+            loggedUser: postId,
           },
         })
           .then((post) => {
@@ -87,35 +87,35 @@ exports.deleteMyAccount = (req, res) => {
             // Ic on trouve tous les commentaires associé au id de user trovuer plus haut
             Comment.findAll({
               where: {
-                idUser: commentId,
+                loggedUser: commentId,
               },
             })
               .then((comment) => {
                 // console.log("Tous les posts de l'utilisateur trouvé", comment)
                 // dans le cas ou l'un d'entre eux ont des immages, on supprime les images mais aussi le compte y compris le post et les comments associé
                 if (
-                  user &&
-                  (user.isAdmin ||
-                    user.id == user.idUser ||
-                    user.id == comment.idUser ||
-                    user.id == post.idUser)
+                  userLogged &&
+                  (userLogged.isAdmin ||
+                    userLogged.id == userLogged.loggedUser ||
+                    userLogged.id == comment.loggedUser ||
+                    userLogged.id == post.loggedUser)
                 ) {
                   if (post.imageUrl != null || comment.imageUrl != null) {
                     // On supprime peut importe si il y a l'image ou non (supérieur ou egal a null)
                     const fileNameComment =
                       comment.imageUrl.split("/images/")[1];
                     const fileNamePost = post.imageUrl.split("/images/")[1];
-                    const fileNameUser = user.imageUrl.split("/images/")[1];
+                    const fileNameUser = userLogged.imageUrl.split("/images/")[1];
                     fs.unlink(
                       `images/${(fileNameComment, fileNamePost, fileNameUser)
                       }`,
                       () => {
-                        if (user > null || post >= null || comment >= null) {
+                        if (userLogged > null || post >= null || comment >= null) {
                           // si le user, post et comment il y a des images, on les supprime de la base de donées et du serveur pour l'image
                           // On supprime aussi le post et le comment même si il n'y a rien
                           User.destroy({
                             where: {
-                              id: userId,
+                              id: deletedUser,
                             },
                           })
                             .then((destroyed) => {
