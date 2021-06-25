3f85c7b67a0c1b9f6dfc0e7affd4b69592f1a1b0
David Bouhaben <davidbouhaben@yahoo.co.id>
2021-06-26 00:08:08 +0200

    import fontawesome for vue 3

---
+++ Frontend/DavidBouhaben_7_groupomania_fullstack/src/plugins/font-awesome.js
@@ -0,0 +1,9 @@
+import { library  } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+    faHome,
+    faUser,
+    faUserPlus,
+    faSignInAlt,
+    faSignOutAlt,
+}
