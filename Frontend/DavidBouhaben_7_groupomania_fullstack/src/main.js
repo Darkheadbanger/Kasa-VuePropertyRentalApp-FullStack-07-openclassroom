3224e908c64943bf228a82ad995c646e79f3f71f
David Bouhaben <davidbouhaben@yahoo.co.id>
2021-06-26 14:31:21 +0200

    importation finish

--- Frontend/DavidBouhaben_7_groupomania_fullstack/src/main.js
+++ Frontend/DavidBouhaben_7_groupomania_fullstack/src/main.js
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-vue/dist/bootstrap-vue.css"
+import { FontAwesomeIcon } from "./plugins/font-awesome";
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(router)
+  .use(store)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
