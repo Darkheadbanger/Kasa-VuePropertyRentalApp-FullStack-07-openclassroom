ca42adac376e2e283c6e2ea566c0402b18b78637
David Bouhaben <davidbouhaben@yahoo.co.id>
2021-07-04 01:43:26 +0200

    global scsss dosen't work

--- Backend/routes/users.routes.js
+++ Backend/routes/users.routes.js
@@ -5,7 +5,7 @@ const multer = require("../middleware/multer-config")
 const router = express.Router()
 
 router.get('/', auth, authCtrl.findAllUsers)
-router.get('/me/:id', auth, authCtrl.userProfil);
+router.get('/me/:id', auth, authCtrl.userProfil);//localhost/3000/api/auth/me/id
 router.put('/me/:id', auth, multer, authCtrl.updateUser)
 //Un user peut effacer son compte, un admin peut effacer son compte et les comptes des autres
 router.delete('/me/:id', auth, authCtrl.deleteMyAccount);
