b19238c8760fd2275740b4ef33c4236b834e4828
David Bouhaben <davidbouhaben@yahoo.co.id>
2021-07-16 11:31:20 +0200

    edit page finish

--- frontend/src/views/Update.vue
+++ frontend/src/views/Update.vue
@@ -91,7 +91,9 @@
             </div>
           </div>
         </div>
-        <div class="row profile-body d-flex flex-row justify-content-center">
+        <div
+          class="row profile-body d-flex flex-row justify-content-center ms-5"
+        >
           <!-- Pour mettre de publication -->
           <!-- middle wrapper start -->
           <div class="col-xl-6 middle-wrapper">
@@ -99,27 +101,163 @@
               <div class="col-md-12">
                 <div class="card rounded">
                   <div class="card-header">
-                    <div
-                      class="d-flex flex-column justify-content-start"
-                    >
-                      <div
-                        class="
-                          d-flex
-                          justify-content-start
-                          border-bottom
-                        "
-                      >
+                    <div class="d-flex flex-column justify-content-start">
+                      <div class="d-flex justify-content-start border-bottom">
                         <div class="ml-2 d-flex me-3">
                           <span class="mt-3 mb-3"
                             >Paramètres généraux du compte</span
                           >
                         </div>
                       </div>
-                      <div class="ml-2 d-flex me-3">
-                        <span class="mt-3 mb-3"
-                          >Paramètres généraux du compte</span
-                        >
+                      <div
+                        class="
+                          d-flex
+                          justify-content-between
+                          border-bottom
+                          edit-row edit-row--transition
+                        "
+                      >
+                        <div class="ml-2 d-flex">
+                          <span class="mt-3 mb-3">Prénom :</span>
+                        </div>
+                        <div class="ml-2 d-flex">
+                          <span class="mt-3 mb-3">{{ user.firstName }}</span>
+                        </div>
+                        <div class="ml-2 d-flex">
+                          <div class="col-sm-6 col-md-9 mt-2">
+                            <input
+                              class="form-control mr-sm-2 bg-light"
+                              type="text"
+                              placeholder="Edit"
+                              aria-label="Edit"
+                              id="searchText"
+                            />
+                          </div>
+                        </div>
+                        <div class="mt-2">
+                          <button
+                            class="
+                              btn btn-primary btn-icon-text btn-edit-profile
+                            "
+                            @click="updateFirstName"
+                          >
+                            <font-awesome-icon :icon="['fas', 'edit']" />
+                            Edit
+                          </button>
+                        </div>
                       </div>
+                      <div class="d-flex justify-content-between border-bottom">
+                        <div class="ml-2 d-flex">
+                          <span class="mt-3 mb-3">Nom :</span>
+                        </div>
+                        <div class="ml-2 d-flex">
+                          <span class="mt-3 mb-3">{{ user.lastName }}</span>
+                        </div>
+                        <div class="ml-2 d-flex">
+                          <div class="ml-2 d-flex">
+                            <div class="col-sm-6 col-md-9 mt-2">
+                              <input
+                                class="form-control mr-sm-2 bg-light"
+                                type="text"
+                                placeholder="Edit"
+                                aria-label="Edit"
+                                id="searchText"
+                              />
+                            </div>
+                          </div>
+                        </div>
+                        <div class="mt-2">
+                          <button
+                            class="
+                              btn btn-primary btn-icon-text btn-edit-profile
+                            "
+                            @click="updateFirstName"
+                          >
+                            <font-awesome-icon :icon="['fas', 'edit']" />
+                            Edit
+                          </button>
+                        </div>
+                      </div>
+                      <div class="d-flex justify-content-between border-bottom">
+                        <div class="ml-2 d-flex">
+                          <span class="mt-3 mb-3">Pseudeo :</span>
+                        </div>
+                        <div class="ml-2 d-flex">
+                          <span class="mt-3 mb-3">{{ user.userName }}</span>
+                        </div>
+                        <div class="ml-2 d-flex">
+                          <div class="ml-2 d-flex">
+                            <div class="col-sm-6 col-md-9 mt-2">
+                              <input
+                                class="form-control mr-sm-2 bg-light"
+                                type="text"
+                                placeholder="Edit"
+                                aria-label="Edit"
+                                id="searchText"
+                              />
+                            </div>
+                          </div>
+                        </div>
+                        <div class="mt-2">
+                          <button
+                            class="
+                              btn btn-primary btn-icon-text btn-edit-profile
+                            "
+                            @click="updateFirstName"
+                          >
+                            <font-awesome-icon :icon="['fas', 'edit']" />
+                            Edit
+                          </button>
+                        </div>
+                      </div>
+                      <div class="d-flex justify-content-between border-bottom">
+                        <div class="ml-2 d-flex">
+                          <span class="mt-3 mb-3">Email :</span>
+                        </div>
+                        <div class="ml-2 d-flex">
+                          <span class="mt-3 mb-3">{{ user.email }}</span>
+                        </div>
+                        <div class="ml-2 d-flex">
+                          <div class="ml-2 d-flex">
+                            <div class="col-sm-6 col-md-9 mt-2">
+                              <input
+                                class="form-control mr-sm-2 bg-light"
+                                type="text"
+                                placeholder="Edit"
+                                aria-label="Edit"
+                                id="searchText"
+                              />
+                            </div>
+                          </div>
+                        </div>
+                        <div class="mt-2">
+                          <button
+                            class="
+                              btn btn-primary btn-icon-text btn-edit-profile
+                            "
+                            @click="updateFirstName"
+                          >
+                            <font-awesome-icon :icon="['fas', 'edit']" />
+                            Edit
+                          </button>
+                        </div>
+                      </div>
+                      <!-- <div class="d-flex justify-content-between border-bottom">
+                        <div class="ml-2 d-flex">
+                          <span class="mt-3 mb-3">Admin :</span>
+                        </div>
+                        <div class="ml-2 d-flex">
+                          <span class="mt-3 mb-3">{{ user.isZdmin }}</span>
+                        </div>
+                        <div class="ml-2 d-flex">
+                          <input
+                            class="form-check-input mt-0"
+                            type="checkbox"
+                            value=""
+                            aria-label="Checkbox for following text input"
+                          />
+                        </div>
+                      </div> -->
                     </div>
                   </div>
                 </div>
