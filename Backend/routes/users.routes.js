c18a24518d07f715d72417053f9103d59242ec4b
David Bouhaben <davidbouhaben@yahoo.co.id>
2021-07-28 12:59:05 +0200

    patch for updateprofil

--- Backend/routes/users.routes.js
+++ Backend/routes/users.routes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const authCtrl = require("../controllers/user.controller");
 const auth = require("../middleware/auth.jwt");
-const multer = require("../middleware/multer-config");
 const router = express.Router();
 
 router.get("/", auth, authCtrl.findAllUsers);
 router.get("/me/:id", auth, authCtrl.userProfil); //localhost/3000/api/account/me/id
-router.patch("/me/:id", auth, multer, authCtrl.updateUser);
+router.put("/me/:id", auth, authCtrl.updateUser);
+// Patch sert à modifier un seul truc, 
+router.patch("/me", auth, authCtrl.updateProfil);
 //Un user peut effacer son compte, un admin peut effacer son compte et les comptes des autres
 router.delete("/me/:id", auth, authCtrl.deleteMyAccount);
 
