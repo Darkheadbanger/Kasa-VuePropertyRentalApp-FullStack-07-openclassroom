ec828870f439bd3777383fb27cece61dd8172ace
David Bouhaben <davidbouhaben@yahoo.co.id>
2021-07-29 09:51:06 +0200

    getAllMyPost works

--- Backend/routes/users.routes.js
+++ Backend/routes/users.routes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get("/", auth, authCtrl.findAllUsers);
 router.get("/me/:id", auth, authCtrl.userProfil); //localhost/3000/api/account/me/id
 router.put("/me/:id", auth, authCtrl.updateUser);
-// Patch sert à modifier un seul truc, 
+// Patch sert à modifier un, 
 router.patch("/me", auth, authCtrl.updateProfil);
 //Un user peut effacer son compte, un admin peut effacer son compte et les comptes des autres
 router.delete("/me/:id", auth, authCtrl.deleteMyAccount);
