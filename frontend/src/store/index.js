eca68c8137f8a0ce162ea54c58617e3cdfb3f7ac
David Bouhaben <davidbouhaben@yahoo.co.id>
2021-08-04 15:42:56 +0200

    update comment and post works only on first id

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -381,12 +381,14 @@ export default createStore({
         if (state.posts[i].id == post.id) {
           console.log(state.posts[i].postContent, "==", post.postContent);
           // Si l'image existe, on n'efface pas l'image quand on écrit le texte, si le texte existe on efface pas le texte si on change l'image
-          if (state.posts[i].postContent && state.posts[i].imageUrl) {
-            //  Implementation de la logique update de bon post à partir de son Id
-            state.posts[i].postContent = post.postContent;
-            state.posts[i].imageUrl = post.imageUrl;
-            state.posts[i].updatedAt = post.updatedAt;
-          }
+          // if (state.posts[i].postContent && state.posts[i].imageUrl) {
+          // }
+          //  Implementation de la logique update de bon post à partir de son Id
+          state.posts[i].postContent = post.postContent;
+          state.posts[i].imageUrl = post.imageUrl;
+          state.posts[i].updatedAt = post.updatedAt;
+
+          // state.posts[i] = post;
         }
       }
     },
@@ -405,14 +407,22 @@ export default createStore({
           // Une condition pour comparer l'id de comments existant et nouvelle comments
           if (beforeUpdateComment.id == afterUpdateComment.id) {
             console.log("Do update");
-            console.log(beforeUpdateComment, "==", afterUpdateComment);
+            console.log(
+              beforeUpdateComment.comment,
+              "==",
+              afterUpdateComment.comment
+            );
             // Si l'image existe, on n'efface pas l'image quand on écrit le texte, si le texte existe on efface pas le texte si on change l'image
-            if (beforeUpdateComment.imageUrl && beforeUpdateComment.comment) {
-              // Implementation de la logique update commentaire, image et update at
-              beforeUpdateComment.comment = afterUpdateComment.comment;
-              beforeUpdateComment.imageUrl = afterUpdateComment.imageUrl;
-              beforeUpdateComment.updateAt = afterUpdateComment.updateAt;
-            }
+            // if (beforeUpdateComment.imageUrl && beforeUpdateComment.comment) {
+            // }
+            // Implementation de la logique update commentaire, image et update at
+
+            console.log(
+              (beforeUpdateComment.comment = afterUpdateComment.comment)
+            );
+            console.log(
+              (beforeUpdateComment.imageUrl = afterUpdateComment.imageUrl)
+            );
             // statePost.comments[i] = comment.comments
           }
         }
